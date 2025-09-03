3519607dcc1dd239757b76046f6e0804b1c4e69b
ar-vera <va.ryabova24@gmail.com>
2025-09-03 17:46:41 +0300

    Transactions added

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"Order-tracker-service/internal/db"
+	"Order-tracker-service/internal/domain"
+	"Order-tracker-service/internal/repository"
 	"fmt"
 	"github.com/jmoiron/sqlx"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"log"
 	"os"
 )
@@ -24,6 +26,34 @@ func main() {
 	}
 	defer dataBase.Close()
 
-	db.RunMigrations(dataBase, "/Users/veraryabova/Desktop/Go/Order-tracker-service/migrations/")
+	// db.RunMigrations(dataBase, "/Users/veraryabova/Desktop/Go/Order-tracker-service/migrations/")
 
+	repo := repository.NewOrderRepository(dataBase)
+
+	// ⚡ Тестовые данные
+	order := domain.GetTestOrder()
+
+	// --- Create ---
+	//err = repo.Create(order)
+	if err != nil {
+		log.Fatal("Ошибка при сохранении заказа:", err)
+	}
+	fmt.Println("✅ Order успешно сохранён в БД")
+
+	// --- GetById ---
+	orderFromDb, err := repo.GetById(order.OrderUID)
+	if err != nil {
+		log.Fatal("Ошибка при получении заказа по ID:", err)
+	}
+	fmt.Printf("📦 Order по UID=%s:\n%+v\n\n", order.OrderUID, orderFromDb)
+
+	// --- GetAll ---
+	allOrders, err := repo.GetAll()
+	if err != nil {
+		log.Fatal("Ошибка при получении всех заказов:", err)
+	}
+	fmt.Println("📜 Все заказы из БД:")
+	for _, o := range allOrders {
+		fmt.Printf("- %s (%s)\n", o.OrderUID, o.CustomerID)
+	}
 }
